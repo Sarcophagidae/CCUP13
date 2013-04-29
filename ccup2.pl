159c55b91cf04a6fcbf1bf88e8682340f71e3319
Sarcophagidae <ergate@seccat.tcinet.ru>
2013-04-29 17:54:21 +0400

    Added softcore(brutforce) variant for ex#2 (dont work correct); Added third parametr in ccup_test => if there is third parametr then path to folder with tests will taked like path to single test and script  will test it

--- ccup2.pl
+++ ccup2.pl
@@ -16,48 +16,134 @@ sub cmn{
 	fact($n_)/(fact($n_-$m_)*fact($m_));
 }
 	
-#print cmn(3,2),' ',cmn(4,2);
-#exit;
-
 sub checkTrap{
 	my $a = $_[0];
 	my $x = $_[1];
 	my $y = $_[2];
 
-	if ($y>$x){
+	#say "$a $x $y";
+	if ($y<$x){
 		my $tmp = $x;
 		$x = $y;
 		$y = $x;
 	}
 	
-	my $z = 
+	my $z = ($y - $x) / 2;
+	#print "z = $z\n";
+	if ($a*$a - $z*$z <= 0){
+		return 0;
+	} else {return 1;}
 }
 
-my $questCnt = <>;
-my $line;
-for (my $i = 1; $i <= $questCnt; $i ++){
-	say "-------------";
-	my $sideCnt = <>;
-	my @sides = split (' ',<>);
+sub isFourEqual{
+	my ($a1, $a2, $a3, $a4) = @_;
+	if (($a1 == $a2) and ($a2==$a3) and ($a3 == $a4)) {
+		1;
+	} else {
+		0;
+	}
+}
+
+sub checkFullTrap{
 	my %g;
-	map {$g{$_}++;} @sides;
-	#map {say "key <$_><$g{$_}>"} keys %g;
+	#say @_;
+	map {$g{$_}++;} @_;
+	my @tmp = grep ($g{$_}>1, keys %g);
+	return 1 if (@tmp == 2);
+	my ($i, $j);
+	for ($i=0; $i<4; $i++){
+		my $sum = 0;
+		for ($j = 0; $j<4; $j++){
+			next if ($i == $j);
+			$sum += $_[$j];	
+		}
+		if ($sum < $_[$i]){
+			return 0;
+		}
+	}
 
-	map {
-		my $tmpk = $_;
-		say "key [$tmpk][$g{$tmpk}]";
-		if ($g{$tmpk}>=4){
-			say "[$tmpk] >= 4";
-			say "\t",cmn(4,$g{$tmpk}); 
+	if (@tmp == 1){
+		if (isFourEqual(@_)){
+			return 1;
+		} else {
+			my @ar = grep ($_!=$tmp[0], keys %g);
+			if (@ar == 1){
+				return checkTrap($tmp[0], $tmp[0], @ar);
+			} else {	
+				return checkTrap($tmp[0], @ar);
+			}
 		}	
-		say "[$tmpk] >= 2";
-		map {
-			say "[$tmpk] x $_";
-			say "\t",cmn(2,$g{$tmpk})*cmn(2,$g{$_}); 
-		} grep (($g{$_}>=2)&&($_!=$tmpk), keys %g);
-	} grep ($g{$_}>=2, keys %g);
-
-	#map {print "$_ $g{$_}\n"} keys %g;
-	#say "------";
-	%g=();	
+	} else { return 0; }
 }
+
+sub hard{
+	my $questCnt = <>;
+	my $line;
+	for (my $i = 1; $i <= $questCnt; $i ++){
+		say "-------------";
+		my $sideCnt = <>;
+		my @sides = split (' ',<>);
+		my %g;
+		map {$g{$_}++;} @sides;
+		#map {say "key <$_><$g{$_}>"} keys %g;
+
+		map {
+			my $tmpk = $_;
+			say "key [$tmpk][$g{$tmpk}]";
+			if ($g{$tmpk}>=4){
+				say "[$tmpk] >= 4";
+				say "\t",cmn(4,$g{$tmpk}); 
+			}	
+			say "[$tmpk] >= 2";
+			map {
+				say "[$tmpk] x $_";
+				say "\t",cmn(2,$g{$tmpk})*cmn(2,$g{$_}); 
+			} grep (($g{$_}>=2)&&($_!=$tmpk), keys %g);
+		} grep ($g{$_}>=2, keys %g);
+
+		#map {print "$_ $g{$_}\n"} keys %g;
+		#say "------";
+		%g=();	
+	}
+}
+
+sub soft{
+	my $questCnt = <>;
+	my $line;
+	for (my $i = 1; $i <= $questCnt; $i ++){
+		my $cnt = 0;
+		my $cntYes = 0;
+		my $sideCnt = <>;
+		my @sides = split (' ',<>);
+		if (@sides == 4) {
+			$cntYes = checkFullTrap(@sides); 
+		} else {
+			for (my $j1 = 0; $j1< @sides; $j1++){	
+				for (my $j2 = $j1; $j2< @sides; $j2++){	
+					next if ($j2 <= $j1);
+					for (my $j3 = $j2; $j3< @sides; $j3++){	
+						next if (($j3 <= $j2)or($j3 <= $j1));
+						for (my $j4 = $j3; $j4< @sides; $j4++){	
+							next if (($j4 <= $j3)or($j4 <= $j2)or($j4 <= $j1));
+							$cntYes++ if (checkFullTrap(@sides[$j1,$j2,$j3,$j4]));
+							$cnt++;
+						}
+					}
+				}
+			}
+		}
+	say "$cntYes";
+	} 
+}
+#say checkTrap(5,3,9);
+#say checkTrap(5,9,3);
+#say checkTrap(4,2,10);
+#say checkTrap(4,4,4);
+
+#print checkFullTrap(1,2,10,10)."\n"; # 1
+#print checkFullTrap(1,2,2,10)."\n"; 	# 0
+#print checkFullTrap(10,10,10,10)."\n";#1
+#print checkFullTrap(1,2,1,150)."\n";	# 0
+#print checkFullTrap(10,2,10,10)."\n";# 1
+#say cmn(4,7); exit;
+soft;
--- ccup_test.pl
+++ ccup_test.pl
@@ -14,10 +14,14 @@ sub oneTest{
 		my $correctResult= do {local $/ = undef; <FILE>;};
 		close FILE;
 		my @sres = split ("\n", $scriptResult);
-		my @cres = split ("\r\n", $correctResult);
-	
+		my @cres;
+		if ($correctResult =~ /\r\n/){
+			@cres = split ("\r\n", $correctResult);
+		} else {
+			@cres = split ("\n", $correctResult);
+		}
 		if (@sres != @cres){
-			print "Test '$testPath': results count are not equal";
+			print "Test '$testPath': results count are not equal\n";
 		} else {
 			my $fails = 0;
 			for (my $i=0; $i<@sres; $i++){
@@ -33,15 +37,18 @@ sub oneTest{
 			}	else { print "Test '$testPath' was failed\n";}
 		}
 		0;
-	}
+	} else {print"Can nooooooot locate file\n";}
 	-1;
 }
 
 my $testScript = $ARGV[0];
 my $testFolder = $ARGV[1];
+my $testOneScript = $ARGV[2];
 
-
-oneTest('/sdom/tmp/ccup1.pl','/sdom/tmp/01','/sdom/tmp/01.a');
+if (defined $testOneScript){
+	oneTest($testScript,$testFolder,$testFolder.'.a');
+	exit;
+}
 
 my $flag = 1;
 my $index = 1;
